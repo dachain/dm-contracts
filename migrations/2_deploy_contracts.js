1cdabeeb292af17d6944ef02696ca93f66eaf804
dachain <35323946+dachain@users.noreply.github.com>
2018-03-01 15:25:36 +0800

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,12 +1,7 @@
-// var StandardToken = artifacts.require("./lib/StandardToken.sol");
-// var Token = artifacts.require("./lib/Token.sol");
-var DMToken = artifacts.require("./DAChainToken.sol");
+var DAChainToken = artifacts.require("./DAChainToken.sol");
 
 var accounts = web3.eth.accounts
 
 module.exports = function(deployer) {
-  // deployer.deploy(Token);
-  // deployer.deploy(StandardToken);
-  deployer.deploy(DMToken);
-
+  deployer.deploy(DAChainToken);
 };
